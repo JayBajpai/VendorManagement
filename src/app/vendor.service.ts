0506439da6f02fdea6696ac1a9d613da70f5fb54
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2024-05-24 15:35:58 +0530

    Update vendor.service.ts

--- src/app/vendor.service.ts
+++ src/app/vendor.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class VendorService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/vendors';
+
+  constructor(private http:HttpClient) { }
+
+  getAllVendors() {
+    return this.http.get(`${this.baseUrl}/getAllVendors`);
+  }
+
+  addVendor(vendor: any) {
+    return this.http.post(`${this.baseUrl}/addVendor`, vendor);
+  }
 }
