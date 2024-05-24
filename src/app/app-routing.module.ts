86fe35bf4fc7612b8cbd758af3e87889c602c130
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2024-05-24 15:39:41 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './employees/employees.component';
+import { VendorsComponent } from './vendors/vendors.component';
+import { EmailsComponent } from './emails/emails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employee', component: EmployeesComponent },
+  { path: 'vendor', component: VendorsComponent},
+  { path: 'email', component: EmailsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
