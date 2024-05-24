b131992b770d4ba5102751ab0ea0196f0921596c
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2024-05-24 15:35:19 +0530

    Update vendors.component.ts

--- src/app/vendors/vendors.component.ts
+++ src/app/vendors/vendors.component.ts
@@ -1,10 +1,55 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { VendorService } from '../vendor.service';
 
 @Component({
   selector: 'app-vendors',
   templateUrl: './vendors.component.html',
   styleUrls: ['./vendors.component.css']
 })
-export class VendorsComponent {
+export class VendorsComponent implements OnInit {
+
+  displayedColumns: string[] = ['email', 'name', 'upi'];
+  vendors: any[] = [];
+  newVendor = { email: '', name: '', upi: '' };
+  isLoading = false;
+error: string = '';
+
+  constructor(private vendorService: VendorService) { }
+
+  ngOnInit(): void {
+    this.loadVendors();
+  }
+
+  loadVendors() {
+    this.isLoading = true;
+    this.vendorService.getAllVendors().subscribe(
+      (data: any) => {
+        this.vendors = data;
+        this.isLoading = false;
+      },
+      error => {
+        this.isLoading = false;
+        this.error = error.message
+       // console.log('Error fetching vendors:', error);
+      }
+    );
+  }
+
+  addVendor() {
+    this.vendorService.addVendor(this.newVendor).subscribe(
+      (data: any) => {
+        console.log('Vendor added successfully:', data);
+
+        this.loadVendors();
+
+        this.newVendor = { email: '', name: '', upi: '' };
+      },
+      error => {
+        console.log('Error adding vendor:', error);
+      }
+    );
+  }
+}{
 
 }
