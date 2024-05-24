b6ee68ebe689d733b3da40360c1952f877c5d20a
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2024-05-24 15:42:38 +0530

    Update emails.component.ts

--- src/app/emails/emails.component.ts
+++ src/app/emails/emails.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-emails',
   templateUrl: './emails.component.html',
   styleUrls: ['./emails.component.css']
 })
-export class EmailsComponent {
+export class EmailsComponent implements OnInit {
+
+  displayedColumns: string[] = ['recipient', 'subject', 'body'];
+  dataSource: any[] = [];
+  newEmail: any = {};
+
+  constructor(private emailService: EmailService) { }
+
+  ngOnInit(): void {
+    this.loadEmails();
+  }
+
+  loadEmails() {
+    this.emailService.getAllEmails().subscribe(
+      (data: any) => {
+        this.dataSource = data;
+      },
+      error => {
+        console.log('Error fetching emails:', error);
+      }
+    );
+  }
+
+  sendEmail() {
+    this.emailService.sendEmail(this.newEmail).subscribe(
+      (data: any) => {
+        console.log('Email sent successfully:', data);
+
+        this.loadEmails();
+
+        this.newEmail = {};
+      },
+      error => {
+        console.log('Error sending email:', error);
+      }
+    );
+  }
+}{
 
 }
