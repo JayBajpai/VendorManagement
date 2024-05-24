89290eb20ef3fe662bb203bc055259269e6e8025
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2024-05-24 15:37:17 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+import {VendorsComponent} from './vendors/vendors.component'
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EmailsComponent } from './emails/emails.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    VendorsComponent,
+    EmailsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    FormsModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
     AppRoutingModule
   ],
   providers: [],
